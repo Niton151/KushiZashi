86f3ffea23dbd2f285ec2b9d8a6d2920af4f3bbb
Nito <niton984@gmail.com>
2022-03-27 21:45:44 +0900

    あとは音とか見た目とかの段階

---
+++ Assets/KushiZashi/Scripts/SoundManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public AudioClip bgm;
+    public AudioClip clock;
+    public AudioClip gusa;
+    public AudioClip ju;
+    public AudioClip match;
+    public AudioClip wrong;
+    public AudioClip money;
+    public AudioClip timeup;
+    public AudioClip delete;
+    public AudioClip select;
+    public AudioClip upgrade;
+    public AudioClip unlock;
+}
